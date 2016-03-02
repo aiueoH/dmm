24e3a83f9d1d44086895fab15eb4bb19a587bb12
GeorgioWan <georgio_sky2000@hotmail.com>
2016-03-02 05:31:32 +0000

    Add validation for item name and reciver name, adjust some empty column display information

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
     has_many :orders
+    
+    validates_presence_of   :name, message: "請務必填入物資名稱。"
+    validates_uniqueness_of :name, message: "此名稱已被使用。"
 end
--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -1,3 +1,6 @@
 class Receiver < ActiveRecord::Base
     has_many :orders
+    
+    validates_presence_of   :name, message: "請務必填入接收單位名稱。"
+    validates_uniqueness_of :name, message: "此名稱已被使用。"
 end
